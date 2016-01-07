23768a64406f3fe5371952e782911932fde623ef
tornike abramishvili <tornikeabramishvili@tornikes-mac.local>
2016-01-07 06:31:33 -0800

    divagate signIncontroller

---
+++ MovieChallange/SigninViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SigninViewController.swift
+//  MovieChallange
+//
+//  Created by tornike abramishvili on 1/7/16.
+//  Copyright © 2016 tornike abramishvili. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    @IBAction func signIn(sender: UIButton) {
+        
+    }
+}
