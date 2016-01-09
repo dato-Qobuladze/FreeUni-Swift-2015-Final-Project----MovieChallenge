e52f860cc4b9dad8baa239293fbc685dfff69755
Giorgi Rokhadze <giorgirokhadze@giorgis-mac.local>
2016-01-09 04:05:57 -0800

    Trying to play audio file

--- MovieChallange/AudioQuestionViewController.swift
+++ MovieChallange/AudioQuestionViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import AVFoundation
 
 class AudioQuestionViewController: UIViewController {
 
@@ -17,22 +19,58 @@ class AudioQuestionViewController: UIViewController {
     @IBOutlet weak var answerC: UIButton!
     @IBOutlet weak var answerD: UIButton!
     
+    private var audioPlayer: AVAudioPlayer!
+    private var player: AVPlayer!
     
     @IBAction func answered(sender: UIButton) {
         if (sender.isEqual(answerA)) {
-            print("shemovida e chemisa")
+            print("shemovida a chemisa")
+        }
+        if (sender.isEqual(answerB)) {
+            print("shemovida b chemisa")
+        }
+        if (sender.isEqual(answerC)) {
+            print("shemovida c chemisa")
+        }
+        if (sender.isEqual(answerD)) {
+            print("shemovida d chemisa")
         }
     }
     
     @IBAction func next(sender: UIButton) {
+        
     }
     
     @IBAction func playMusic(sender: AnyObject) {
+        let query = PFQuery(className: "AudioFile")
+        query.whereKeyExists("audio")
+        query.findObjectsInBackgroundWithBlock { (objects:[PFObject]?, error: NSError?) -> Void in
+            if error == nil {
+                let audio = objects?[0]
+                let audioFile = audio?["audio"] as? PFFile
+                let filePath = audioFile?.url
+                let url = NSURL(fileURLWithPath: filePath!)
+                let soundData = NSData(contentsOfURL: url)
+                if soundData != nil {
+                    do {
+                        self.audioPlayer = try AVAudioPlayer(data: soundData!) // AVAudioPlayer(contentsOfURL: url)
+                        self.audioPlayer.prepareToPlay()
+                        self.audioPlayer.volume = 1.0
+                        self.audioPlayer.play()
+                    } catch {
+                        print("Error occured while playing music!")
+                    }
+                } else {
+                    print("Error occured while getting data!")
+                }
+            } else {
+                print("Error occured while downloading file!")
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
