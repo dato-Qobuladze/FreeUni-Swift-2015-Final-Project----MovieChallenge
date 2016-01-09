5ca14c6a0b07e7c585ee9a96deb2c4eec932f47b
Giorgi Rokhadze <giorgirokhadze@giorgis-mac.local>
2016-01-09 08:33:51 -0800

    Created ImageQuestionViewController

---
+++ MovieChallange/ImageQuestionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageQuestionViewController.swift
+//  MovieChallange
+//
+//  Created by Giorgi Rokhadze on 1/9/16.
+//  Copyright © 2016 tornike abramishvili. All rights reserved.
+//
+
+import UIKit
+
+class ImageQuestionViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
