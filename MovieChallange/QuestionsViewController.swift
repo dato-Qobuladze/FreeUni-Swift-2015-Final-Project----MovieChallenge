3af2da7baf284a3cb6fe30058ad8af177d7a3ac8
tornike abramishvili <tornikeabramishvili@tornikes-mac.local>
2016-01-10 05:05:36 -0800

    davamate timer kitxvebis scenaze

--- MovieChallange/QuestionsViewController.swift
+++ MovieChallange/QuestionsViewController.swift
@@ -10,23 +10,30 @@ import UIKit
 
 class QuestionsViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
+    @IBOutlet weak var timer_lbl: UILabel!
     @IBOutlet weak var pagerView: UIView!
-    @IBOutlet weak var pagerControlls: UIPageControl!
+    @IBOutlet weak var pagerControls: UIPageControl!
+    
+    var timeElapsed: Int = 0 {
+        didSet{
+            let hours = String(format: "%02d", timeElapsed/60/60)
+            let minutes = String(format: "%02d", timeElapsed/60%60)
+            let seconds = String(format: "%02d", timeElapsed%60)
+            
+            timer_lbl.text = "\(hours):\(minutes):\(seconds)"
+        }
+    }
     
     var pageViewController: UIPageViewController?
     var questionViewControllers: [UIViewController]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let q1 = storyboard?.instantiateViewControllerWithIdentifier("q_simple")
-        let q2 = storyboard?.instantiateViewControllerWithIdentifier("q_image")
-        let q3 = storyboard?.instantiateViewControllerWithIdentifier("q_audio")
-        let q4 = storyboard?.instantiateViewControllerWithIdentifier("q_image")
-        questionViewControllers = [q1!, q2!, q3!, q4!]
+        loadQuestionsViewControllers()
         
         pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
         pageViewController!.dataSource = self
+        pageViewController?.delegate = self
         
         pageViewController!.setViewControllers([questionViewControllers![0]], direction: .Forward, animated: false, completion: nil)
         pageViewController!.view.frame = CGRectMake(0, 0, pagerView.frame.size.width, pagerView.frame.size.height);
@@ -34,7 +41,23 @@ class QuestionsViewController: UIViewController, UIPageViewControllerDataSource,
         addChildViewController(pageViewController!)
         pagerView.addSubview(pageViewController!.view)
         pageViewController!.didMoveToParentViewController(self)
-
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "timerTick", userInfo: nil, repeats: true)
+    }
+    
+    func timerTick(){
+        timeElapsed++
+    }
+    
+    func loadQuestionsViewControllers(){
+        let q1 = storyboard?.instantiateViewControllerWithIdentifier("q_simple")
+        let q2 = storyboard?.instantiateViewControllerWithIdentifier("q_image")
+        let q3 = storyboard?.instantiateViewControllerWithIdentifier("q_audio")
+        let q4 = storyboard?.instantiateViewControllerWithIdentifier("q_image")
+        questionViewControllers = [q1!, q2!, q3!, q4!]
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
@@ -54,8 +77,11 @@ class QuestionsViewController: UIViewController, UIPageViewControllerDataSource,
     }
     
     func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        
+        let currentController = pageViewController.viewControllers?.first
+        let index = questionViewControllers?.indexOf(currentController!)
+        pagerControls.currentPage = index!
     }
+    
     
     func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int
     {
