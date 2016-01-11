f12cb123cd8a57e70ce9663bb1892a344bfbf075
dato-Qobuladze <davidkobuladzemail@gmail.com>
2016-01-11 07:50:51 -0800

    rating controller

--- MovieChallange/RatingViewController.swift
+++ MovieChallange/RatingViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class RatingViewController: UIViewController {
 
+    @IBOutlet weak var userRatingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var multiplayerHistoryTable: UITableView!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
