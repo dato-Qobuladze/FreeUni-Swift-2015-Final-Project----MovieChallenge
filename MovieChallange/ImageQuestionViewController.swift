c4d214de7f32fce79209910a5efb7ca38498c2df
Giorgi Rokhadze <giorgirokhadze@giorgis-mac.local>
2016-01-12 05:05:38 -0800

    Implementing image view question.

--- MovieChallange/ImageQuestionViewController.swift
+++ MovieChallange/ImageQuestionViewController.swift
@@ -11,12 +11,22 @@ import Parse
 
 class ImageQuestionViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let query = PFQuery(className: "AudioFile")
+        
     }
 
 }
