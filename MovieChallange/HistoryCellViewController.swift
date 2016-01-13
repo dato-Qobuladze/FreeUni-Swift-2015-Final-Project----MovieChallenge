fec3f3ab848922c6d863c28f22ab5a467c729722
dato-Qobuladze <davidkobuladzemail@gmail.com>
2016-01-13 05:28:05 -0800

    rating tableview

---
+++ MovieChallange/HistoryCellViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryCellViewController.swift
+//  MovieChallange
+//
+//  Created by dato qobuladze on 1/13/16.
+//  Copyright © 2016 tornike abramishvili. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCellViewController: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}
