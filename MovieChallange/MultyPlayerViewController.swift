3822e5bc3646041d31948e217b4b77d8bfda3aa8
mikheil maghriani <mikheilmaghriani@mikheils-mac.local>
2016-01-13 08:52:19 -0800

    minor changes

--- MovieChallange/MultyPlayerViewController.swift
+++ MovieChallange/MultyPlayerViewController.swift
@@ -31,6 +31,7 @@ class MultyPlayerViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
